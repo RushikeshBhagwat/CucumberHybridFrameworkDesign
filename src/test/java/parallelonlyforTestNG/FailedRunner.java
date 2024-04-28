42d2533508237aa179f7fcd2e2abb5074e468ee5
Rushikesh Bhagwat <bhagwatrushikeshm@gmail.com>
2024-04-28 15:38:42 +0530

    Added one comment in failed runner class

--- src/test/java/parallelonlyforTestNG/FailedRunner.java
+++ src/test/java/parallelonlyforTestNG/FailedRunner.java
@@ -5,7 +5,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 
-
+//This class is created for rerunning the failed cases and the rerun path is given
 
 
 @CucumberOptions(features= "@target/rerun.txt",glue= "parallelonlyforTestNG",plugin= {"pretty","com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:","timeline:test-output-thread/"
